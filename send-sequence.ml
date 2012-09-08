6cddf4bdefb833758f15c8d989c6170e522fc898
Erkki Seppälä <flux@modeemi.fi>
2012-09-08 10:11:00 +0300

    Send-sequence: support sending pre-defined sequences

--- send-sequence.ml
+++ send-sequence.ml
@@ -32,11 +32,76 @@ let write_all fd str =
 type op = 
   | Bool of bool
   | DelayHigh
+  | Sequence of char
 
-let binstr_of_bool = function
+let ops_of_flags flags =
+  let bit_offsets = [
+    0, `A;
+    5, `B;
+    6, `C;
+    1, `D;
+    4, `E;
+    3, `F;
+    2, `G;
+    7, `Dot;
+  ] in
+    List.init 7 **> fun idx ->
+      if List.mem (List.assoc idx bit_offsets) flags
+      then Bool false
+      else Bool true
+
+let sequences = 
+  ['0', [`A; `B;     `D; `E; `F; `G];
+   '1', [            `D;         `G]; (* ?? *)
+   '2', [`A;     `C; `D; `E; `F;   ];
+   '3', [`A;     `C; `D;     `F; `G];
+   '4', [    `B; `C; `D;         `G];
+   '5', [`A; `B; `C;         `F; `G];
+   '6', [`A; `B; `C;     `E; `F; `G];
+   '7', [`A;         `D;         `G]; (* ?? *)
+   '8', [`A; `B; `C; `D; `E; `F; `G];
+   '9', [`A; `B; `C; `D;     `F; `G];
+   ' ', [                          ];
+  ]
+
+(*
+
+- : string = "1?00111"
+# binstr_of_bool (Sequence '1');;
+- : string = "1100111"
+# binstr_of_bool (Sequence '7');;
+- : string = "1000111"
+
+
+- : string = "???????"
+# binstr_of_bool (Sequence '0');;
+- : string = "1000000"
+# binstr_of_bool (Sequence '2');;
+- : string = "0001001"
+# binstr_of_bool (Sequence '3');;
+- : string = "0000011"
+# binstr_of_bool (Sequence '4');;
+- : string = "0100110"
+# binstr_of_bool (Sequence '5');;
+- : string = "0010010"
+# binstr_of_bool (Sequence '6');;
+- : string = "0010000"
+# binstr_of_bool (Sequence '8');;
+- : string = "0000000"
+# binstr_of_bool (Sequence '9');;
+- : string = "0000010"
+# 
+
+*)
+
+
+let rec binstr_of_bool = function
   | Bool false -> "0"
   | Bool true -> "1"
   | DelayHigh -> String.make 12 '1'
+  | Sequence seq -> 
+      try String.concat "" (List.map binstr_of_bool (ops_of_flags (List.assoc seq sequences)))
+      with Not_found -> Printf.ksprintf failwith "unknown sequence %c" seq
 
 let bool_of_binstr = function 
   | '0' -> Some (Bool false)
@@ -71,12 +136,24 @@ let send_sequence fd seq =
 	(* ignore (Core.Core_unix.nanosleep 0.0000520); *)
 
 
-let do_send serial_port sequence =
+let sequence_of_str str =
+  let rec aux = function
+    | [] -> []
+    | 'S'::ch::rest -> Sequence ch::aux rest
+    | ch::rest -> 
+	match bool_of_binstr ch with
+	  | None -> aux rest
+	  | Some bool -> bool::aux rest
+  in
+    List.enum (aux (String.explode str))
+
+let do_send serial_port str =
   let fd = open_serial serial_port 9600 in
-    send_sequence fd (Enum.filter_map bool_of_binstr (String.enum sequence));
+    send_sequence fd (sequence_of_str str);
     Unix.close fd
 
 let main () =
+  Sys.catch_break true;
   let args = List.tl (Array.to_list Sys.argv) in
     match args with
       | serial_port::sequence::[] ->
